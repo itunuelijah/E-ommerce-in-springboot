55ba85cfe40ab002368a182ba05792590b064ac4
itunuelijah <itunuelijah@gmail.com>
2022-02-17 15:40:23 +0100

    product service

--- src/main/java/com/phoenix/service/product/ProductService.java
+++ src/main/java/com/phoenix/service/product/ProductService.java
@@ -1,17 +1,22 @@
 package com.phoenix.service.product;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import com.phoenix.data.dto.ProductDto;
 import com.phoenix.data.models.Product;
 import com.phoenix.web.exceptions.BusinessLogicException;
 import com.phoenix.web.exceptions.ProductDoesNotExistException;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface ProductService {
 
     List<Product> getAllProducts();
     Product findProductById(Long productId) throws ProductDoesNotExistException;
-    Product createProduct(ProductDto productDto) throws BusinessLogicException;
-    Product updateProductDetails(Long productId, JsonPatch productPatch ) throws BusinessLogicException;
+    Product createProduct(ProductDto productDto) throws BusinessLogicException, IOException;
+    Product saveOrUpdate (Product product) throws BusinessLogicException;
+    Product updateProductDetails(Long productId, JsonPatch productPatch )
+            throws ProductDoesNotExistException, BusinessLogicException, JsonPatchException, JsonProcessingException;
 }
