f5ee9075675a5802e8519eb9cd492d91ee4f026e
itunuelijah <itunuelijah@gmail.com>
2022-02-17 15:38:52 +0100

    cart repository

---
+++ src/main/java/com/phoenix/data/repository/CartRepository.java
@@ -0,0 +1,7 @@
+package com.phoenix.data.repository;
+
+import com.phoenix.data.models.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
